bb564dcbf9f5b9146a2fdbc6d7648c0c9a7ace47
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-05-16 21:52:41 +0200

    remove test file, need to be improved

--- test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker build -t coinmon .
-
-
-echo "                       ---                                   "
-echo "Successfully built coinmon from branch $1.                   "
-echo "Commit $2                                                    "
-echo "                       ---                                   "
