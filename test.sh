f78d95b29a276eb17ac0cab066c21d81839ef055
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-05-16 22:17:23 +0200

    test file for travis

---
+++ test.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+docker build -t coinmon .
+
+echo "                       ---                                   "
+echo "Successfully built coinmon from branch $1.                   "
+echo "Commit $2                                                    "
+echo "                       ---                                   "
